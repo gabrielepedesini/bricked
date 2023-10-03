17ec66c4f92a542e34f431d5027569f17ac68ce2
Gabriele Pedesini <125191834+gabrielepedesini@users.noreply.github.com>
2023-10-03 09:55:00 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -386,7 +386,7 @@ touchArea.addEventListener('touchmove', (event) => {
         const touchCurrentX = event.touches[0].clientX;
         const touchDeltaX = touchCurrentX - touchStartX;
 
-        const threshold = 40;
+        const threshold = 30;
 
         if (touchDeltaX < -threshold && !arrowLeftPressed) {
             arrowLeftPressed = true;
@@ -557,7 +557,7 @@ touchArea.addEventListener('touchmove', (event) => {
         const touchCurrentY = event.touches[0].clientY;
         const touchDeltaY = touchCurrentY - touchStartY;
 
-        const threshold = 40;
+        const threshold = 30;
 
         // If the finger swipes down, maintain the delay at 200
         if (touchDeltaY > threshold) {
